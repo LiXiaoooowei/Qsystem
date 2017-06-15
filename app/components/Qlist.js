a91213bbfa92720a41713e65c59b7e4570d33b1e
LiXiaoooowei <lixiaowei0535@gmail.com>
2017-06-15 16:28:37 +0800

    toggle color when new counter-customer relation is added

--- app/components/Qlist.js
+++ app/components/Qlist.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
+var Firebase = require('./FirebaseClient.js');
 
 var headerStyle_large = {
 	"border": "1px solid black",
@@ -16,12 +16,45 @@ var entryStyle = {
 	"padding": "2rem 4rem 0rem 4rem",
 	"font-size": "5rem"
 };
+var entryStyle_blink = {
+    "border": "1px solid black",
+    "border-collapse": "collapse",
+    "padding": "2rem 4rem 0rem 4rem",
+    "font-size": "5rem",
+    "background-color": "red"
+};
 
 var Qlist = React.createClass({
-
+    getInitialState: function() {
+        return {
+            blinking: false
+        }
+    },
+    componentWillMount: function() {
+        var firebaseRef = Firebase.database().ref();
+        var qlistRef = firebaseRef.child('Qlist');
+        qlistRef.on("child_changed", function (snapshot) {
+            this.toggleBgForFixedPeriod();
+        }.bind(this));
+    },
+    toggleBgForFixedPeriod: function() {
+        this.toggleBgColor();
+        setTimeout(()=>{this.setState({blinking: false});
+            clearInterval(this.state.intervalID)}, 10000);
+    },
+    toggleBgColor: function() {
+        var interval = setInterval(() => {var blink=!this.state.blinking;
+            this.setState({
+                intervalID: interval,
+                blinking: blink
+            })},1000);
+    },
+    componentWillUnmount: function() {
+        clearInterval(this.state.intervalID);
+    },
 	render: function() {
 		var length = this.props.queue.length;
-    var counterInfo = new Array();
+        var counterInfo = new Array();
 		var i;
 		for(i=1;i<=4;i++) {
 			if(length-i>=0) {
@@ -31,7 +64,7 @@ var Qlist = React.createClass({
 		return(
 			<table style={headerStyle_large}>
 			<tr>
-			<td style = {entryStyle}>{this.props.queue[length-1]}
+			<td style = {this.state.blinking?entryStyle_blink: entryStyle}>{this.props.queue[length-1]}
 			<br /> <h6>{counterInfo[0]}</h6></td>
 			</tr>
 			<tr>
